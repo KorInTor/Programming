df73abdb5780aeec591fcdca04fb508fbe8f32ad
Danil <pradanilhin@gmail.com>
2022-03-28 14:03:16 +0700

    Added enums to Model Folder

---
+++ Model/Color.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model
+{
+    public enum Color
+    {
+        White,
+        Black,
+        Red,
+        Blue,
+        Yellow
+    }
+}
---
+++ Model/Genre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming.Model
+{
+    public enum Genre
+    {
+        comedy, 
+        drama, 
+        thriller, 
+        action, 
+        horror, 
+        blockbuster
+    }
+}
