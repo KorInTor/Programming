eca873c0e38f5e1e530c63ec9fad6779e54dbc91
Danil Pryahin <pradanilhin@gmail.com>
2024-03-18 23:28:38 +0700

    lab2 done

---
+++ src/Contacts/Contacts/Model/Services/ContactSerializer.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts.Model.Services
+{
+    public static class ContactSerializer
+    {
+        private static string _filePath
+        {
+            get
+            {
+                string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                return Path.Combine(documentsPath, "Contacts", "contacts.json");
+            }
+        }
+
+        public static void SaveContact(Contact contact)
+        {
+            if (!File.Exists(_filePath))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(_filePath));
+            }
+
+            string json = JsonConvert.SerializeObject(contact);
+            File.WriteAllText(_filePath, json);
+        }
+
+        public static Contact LoadContact()
+        {
+            if (!File.Exists(_filePath))
+            {
+                return null;
+            }
+
+            string json = File.ReadAllText(_filePath);
+            try
+            {
+                Contact contact = JsonConvert.DeserializeObject<Contact>(json);
+                return contact;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ src/Contacts/Contacts/ViewModel/LoadCommand.cs
@@ -0,0 +1,27 @@
+﻿using Contacts.Model;
+using Contacts.Model.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Contacts.ViewModel
+{
+    public class LoadCommand : ICommand
+    {
+        public event Action<Contact> ContactLoaded;
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            ContactLoaded?.Invoke(ContactSerializer.LoadContact());
+        }
+    }
+}
